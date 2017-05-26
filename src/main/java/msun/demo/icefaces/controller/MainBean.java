7dcee0ba4ed14425f412266ff07c8e62981f274b
Michael Sun <michael.sun@resmed.com>
2017-05-26 05:09:16 -0700

    adding spring integration

--- src/main/java/msun/demo/icefaces/controller/MainBean.java
+++ src/main/java/msun/demo/icefaces/controller/MainBean.java
@@ -2,42 +2,52 @@ package msun.demo.icefaces.controller;
 
 import java.io.IOException;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.CustomScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import msun.demo.icefaces.bean.Index;
-import msun.demo.icefaces.bean.IndexImpl;
 
 /**
- * Classe controller 
+ * Classe controller
  *
  */
 @ManagedBean(name = "mainBean")
 @CustomScoped(value = "#{window}")
 public class MainBean {
-	
+
 	public final String msg = "main page";
-	
+
 	@Autowired
 	Index index;
-	
+
 	public String printMensagem() {
 		index.setMessage(msg);
 		return index.getMessage();
 	}
 
-	public void menuAction(ActionEvent event){
-        
-        try {
-			FacesContext.getCurrentInstance().
-			getExternalContext().redirect("/icefaces/index.jsf");
+	public void menuAction(ActionEvent event) {
+
+		try {
+			FacesContext.getCurrentInstance().getExternalContext().redirect("/icefaces/index.jsf");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    }
+	}
+
+	@PostConstruct
+	private void init() {
+		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		ServletContext servletContext = (ServletContext) externalContext.getContext();
+		WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext).getAutowireCapableBeanFactory()
+				.autowireBean(this);
+	}
 
 }
